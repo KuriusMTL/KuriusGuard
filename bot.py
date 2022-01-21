af8ee952533ed5a806718700e9ea568cbc427e12
Eduard Anton <eduard.anton2002@gmail.com>
2022-01-20 21:15:55 -0500

    testing and bug fixed

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import discord
 from multicolorcaptcha import CaptchaGenerator
 from io import BytesIO
-from datetime import datetime
+from datetime import datetime, timedelta
 import re
 
-TOKEN = ''
-CAPCTHA_SIZE_NUM = 2
+TOKEN = 'OTIyNzAyMjMyODAwMDg4MDc0.YcFTaw.oGXG_kgQtj5sTt4A0FQW0RpTFZ8'
+CAPCTHA_SIZE_NUM = 1
 
 client = discord.Client(intents=discord.Intents.all())
 captcha = CaptchaGenerator(CAPCTHA_SIZE_NUM)
@@ -18,7 +18,8 @@ captcha_list = {} # ID, {captcha answer, tries}
 
 verified_role = "Verified User"
 
-command_channel = 904198381734330378
+command_channel = 822627889739989026
+server_id = 822627889739989023
 
 @client.event
 async def on_ready():
@@ -32,10 +33,13 @@ async def on_message(message):
         if message.author.id not in captcha_list: # There was no captcha sent to this user
             return
 
-        if message.content == captcha_list[message.author.id]: # Correct answer to captcha
+        if message.content == captcha_list[message.author.id]["answer"]: # Correct answer to captcha
             await message.channel.send("You are now verified and can access the server.")
             captcha_list.pop(message.author.id)
-            await message.author.add_roles(discord.utils.get(message.guild.roles, name=verified_role)) # Give verified role
+
+            server = client.get_guild(server_id)
+            user = get_user_from_server(message.author.id)
+            if user is not None: await user.add_roles(discord.utils.get(server.roles, name=verified_role)) # Give verified role
             return
         
         tries = captcha_list[message.author.id]["tries"] # Get the number of tries left
@@ -57,7 +61,7 @@ async def on_message(message):
             # Should only be used when initializing the bot
             if message.content == "!verifyall":
                 for user in message.guild.members:
-                    user.add_roles(discord.utils.get(message.guild.roles, name=verified_role))
+                    await user.add_roles(discord.utils.get(message.guild.roles, name=verified_role))
                 return
             
             # Flag user manually by ID
@@ -86,7 +90,7 @@ async def on_message(message):
 @client.event
 async def on_member_join(member):
     print(f'{member} has joined the server.')
-    watch_list.append({member.name, member.id, member.joined_at}) # Add member to watch list
+    watch_list.append({"name": member.name, "id": member.id, "joined_at": member.joined_at}) # Add member to watch list
 
     # Keep the watch list length smaller than watch_list_length
     if len(watch_list) > watch_list_length:
@@ -98,21 +102,23 @@ async def on_member_join(member):
     with open("blacklist.txt", "r") as f:
         blacklist = f.readlines()
         if member.name in blacklist:
+            print("Flagged by blacklist")
             await send_captcha(member)
             return
     
     # Check if user joined within 30 seconds of previous user
     if len(watch_list) > 1:
-        if watch_list[-1]["joined_at"] - watch_list[-2]["joined_at"] < datetime.timedelta(seconds=30):
+        if watch_list[-1]["joined_at"] - watch_list[-2]["joined_at"] < timedelta(seconds=30):
+            print("Flagged by joined within 30 seconds")
             await send_captcha(member) # Send captcha to user
             # if previous user hasn't been flagged, flag them and send captcha to them too
             if watch_list[-2]["id"] not in captcha_list.keys():
                 try:
-                    previous_user = await client.fetch_user(watch_list[-2]["id"])
+                    previous_user = get_user_from_server(watch_list[-2]["id"])
                     try:
                         await previous_user.remove_roles(discord.utils.get(member.guild.roles, name=verified_role))
-                    except:
-                        pass
+                    except Exception as e:
+                        print(e)
                     await send_captcha(previous_user)
                 except discord.NotFound:
                     print("Previous user not found")
@@ -121,7 +127,8 @@ async def on_member_join(member):
             return
     
     # Check to see if the user's account is at least a week old
-    if datetime.now() - member.created_at < datetime.timedelta(days=7):
+    if datetime.now() - member.created_at < timedelta(days=7):
+        print("Flagged by ageism")
         await send_captcha(member)
         return
 
@@ -130,29 +137,31 @@ async def on_member_join(member):
         patterns = f.readlines()
         for pattern in patterns:
             if bool(re.match(pattern, member.name)):
+                print("Flagged by patterns")
                 await send_captcha(member)
                 return
     
     # Check if the user has a profile pic or a status
     if member.avatar_url == member.default_avatar_url and member.activity == None:
+        print("Flagged by no profile pic and no status")
         await send_captcha(member)
         return
 
-    member.add_roles(discord.utils.get(member.guild.roles, name=verified_role)) # Give verified role to user
+    await member.add_roles(discord.utils.get(member.guild.roles, name=verified_role)) # Give verified role to user
 
 
 # Send the captcha to user and returns the answer to the captcha
 async def send_captcha(user, tries = max_tries):
     # Remove verified role if user has it
-    if verified_role in user.roles:
+    if hasattr(user, 'roles') and (verified_role in user.roles):
         await user.remove_roles(discord.utils.get(user.guild.roles, name=verified_role))
 
     bytes, answer = generate_captcha()
     img_file = discord.File(bytes, filename="captcha.png")
     emoji = discord.utils.get(client.emojis, name='sus')
-    embed = discord.Embed(title=f"Oops, are you a robot? {str(emoji)}", description="We've noticied some anomalies with your account. There is no need to worry. Please respond with the text in the captcha to verify your identity and access the Kurius Discord server.", color=0xFF0000)
+    embed = discord.Embed(title=f"Oops, are you a robot? {str(emoji)}", description="We've noticed some anomalies with your account. There is no need to worry. Please respond with the 4 digits from the captcha to verify your identity and access the Kurius Discord server.", color=0xFF0000)
     embed.set_image(url='attachment://captcha.png')
-    captcha_list[user.id] = {answer, tries}
+    captcha_list[user.id] = {"answer": answer, "tries": tries}
     await user.send(file=img_file, embed=embed)
     return answer
 
@@ -173,6 +182,14 @@ def generate_captcha():
     bytes.seek(0)
 
     return bytes, characters
+
+def get_user_from_server(user_id):
+    try:
+        server = client.get_guild(server_id)
+        user = server.get_member(user_id)
+        return user
+    except:
+        return None
     
 
 if __name__ == '__main__':
